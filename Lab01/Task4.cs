bca78e64181bf5f313c2737120bee2db59e89bdc
Nest772 <nestkhomra@gmail.com>
2026-09-15 10:21:32 +0300

    Lab01 Task04

---
+++ Lab01/Task4.cs
@@ -0,0 +1,24 @@
+using System;
+
+
+int systolicpressure = int.Parse(Console.ReadLine());
+int dialosticpressure = int.Parse(Console.ReadLine());
+
+
+
+if(systolicpressure< 120 && dialosticpressure < 80 )
+{
+    Console.WriteLine($"{systolicpressure}/{dialosticpressure} norm");
+}
+else if(systolicpressure < 130 && dialosticpressure < 80)
+{
+    Console.WriteLine($"{systolicpressure}/{dialosticpressure} high");
+}
+else if(systolicpressure < 140 || dialosticpressure < 90)
+{
+    Console.WriteLine($"{systolicpressure}/{dialosticpressure} stage 1 hypertension");
+}   
+else
+{
+    Console.WriteLine($"{systolicpressure}/{dialosticpressure} stage 2 hypertension");
+}
