ae4eb3ff3580dbedba46cd70464900a2b2c0d662
Nest772 <nestkhomra@gmail.com>
2026-09-15 15:04:51 +0300

    Update Task3.cs

--- Lab01/Task3.cs
+++ Lab01/Task3.cs
@@ -1,21 +1,26 @@
 using System;
 
-
-int birthdate = int.Parse(Console.ReadLine());
-
-double age = 2026 - birthdate;
-
-Console.WriteLine(age);
-
-if(age <= 17)
+public static class Task3
 {
-    Console.WriteLine("Child");
-}
-else if(age <= 59)
-{
-    Console.WriteLine("Adult");
-}
-else 
-{
-    Console.WriteLine("Pensioner");
+    public static void Run()
+    {
+        int birthdate = int.Parse(Console.ReadLine());
+
+        double age = 2026 - birthdate;
+
+        Console.WriteLine(age);
+
+        if (age <= 17)
+        {
+            Console.WriteLine("Child");
+        }
+        else if (age <= 59)
+        {
+            Console.WriteLine("Adult");
+        }
+        else
+        {
+            Console.WriteLine("Pensioner");
+        }
+    }
 }
