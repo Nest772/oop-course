3da58d3770501875845003eb94b1296c0426190c
Nest772 <nestkhomra@gmail.com>
2026-09-15 15:02:21 +0300

    Update Task5.cs

--- Lab01/Task5.cs
+++ Lab01/Task5.cs
@@ -1,18 +1,23 @@
 using System;
 
-
-int day = int.Parse(Console.ReadLine());
-
-string schedule = day switch
+public static class Task5
 {
-    1 => " monday 08:00 - 18:00",
-    2 => " tuesday 08:00 - 18:00",
-    3 => " wednesday 08:00 - 18:00",
-    4 => " thursday 08:00 - 18:00",
-    5 => " friday 08:00 - 18:00",
-    6 => " saturday 08:00 - 18:00",
-    7 => " sunday 08:00 - 18:00",
-    _ => " incorret number of day",
-};
+    public static void Run()
+    {
+        int day = int.Parse(Console.ReadLine());
 
-Console.WriteLine(schedule);
+        string schedule = day switch
+        {
+            1 => "monday 08:00 - 18:00",
+            2 => "tuesday 08:00 - 18:00",
+            3 => "wednesday 08:00 - 18:00",
+            4 => "thursday 08:00 - 18:00",
+            5 => "friday 08:00 - 18:00",
+            6 => "saturday 08:00 - 18:00",
+            7 => "sunday 08:00 - 18:00",
+            _ => "incorrect number of day"
+        };
+
+        Console.WriteLine(schedule);
+    }
+}
