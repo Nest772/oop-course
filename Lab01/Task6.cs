1aac1ebf117a425b52a0ef3fea50759aaff74362
Nest772 <nestkhomra@gmail.com>
2026-09-15 15:00:58 +0300

    Update Task6.cs

--- Lab01/Task6.cs
+++ Lab01/Task6.cs
@@ -1,21 +1,26 @@
 using System;
 
-
-int cardNumber = int.Parse(Console.ReadLine());
-
-string department = (cardNumber % 10) switch
+public static class Task6
 {
-    0 or 1 => "general therapy",
-    2 or 3=> " surgery",
-    4 or 5 => " cardiology",
-    6 or 7 => " neurology",
-    8 or 9 => " ophthalmology",
-    _ => " incorret",
-};
+    public static void Run()
+    {
+        int cardNumber = int.Parse(Console.ReadLine());
 
-string discount = (cardNumber % 2 == 0) ? "Yes" : "No";
-string checkup = (cardNumber % 3 == 0 || cardNumber % 10 <= 5) ? "Yes" : "No";
+        string department = (cardNumber % 10) switch
+        {
+            0 or 1 => "general therapy",
+            2 or 3 => "surgery",
+            4 or 5 => "cardiology",
+            6 or 7 => "neurology",
+            8 or 9 => "ophthalmology",
+            _ => "incorrect"
+        };
 
-Console.WriteLine($"department: {department}");
-Console.WriteLine($"concession map: {discount}");
-Console.WriteLine($"routine checkup: {checkup}");
+        string discount = (cardNumber % 2 == 0) ? "Yes" : "No";
+        string checkup = (cardNumber % 3 == 0 || cardNumber % 10 <= 5) ? "Yes" : "No";
+
+        Console.WriteLine($"department: {department}");
+        Console.WriteLine($"concession map: {discount}");
+        Console.WriteLine($"routine checkup: {checkup}");
+    }
+}
