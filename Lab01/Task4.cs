acbe8342a976167b456107be686af15fcced16d5
Nest772 <nestkhomra@gmail.com>
2026-09-15 15:03:54 +0300

    Update Task4.cs

--- Lab01/Task4.cs
+++ Lab01/Task4.cs
@@ -1,24 +1,27 @@
 using System;
 
-
-int systolicpressure = int.Parse(Console.ReadLine());
-int dialosticpressure = int.Parse(Console.ReadLine());
-
-
-
-if(systolicpressure< 120 && dialosticpressure < 80 )
+public static class Task4
 {
-    Console.WriteLine($"{systolicpressure}/{dialosticpressure} norm");
-}
-else if(systolicpressure < 130 && dialosticpressure < 80)
-{
-    Console.WriteLine($"{systolicpressure}/{dialosticpressure} high");
-}
-else if(systolicpressure < 140 || dialosticpressure < 90)
-{
-    Console.WriteLine($"{systolicpressure}/{dialosticpressure} stage 1 hypertension");
-}   
-else
-{
-    Console.WriteLine($"{systolicpressure}/{dialosticpressure} stage 2 hypertension");
+    public static void Run()
+    {
+        int systolicpressure = int.Parse(Console.ReadLine());
+        int dialosticpressure = int.Parse(Console.ReadLine());
+
+        if (systolicpressure < 120 && dialosticpressure < 80)
+        {
+            Console.WriteLine($"{systolicpressure}/{dialosticpressure} norm");
+        }
+        else if (systolicpressure < 130 && dialosticpressure < 80)
+        {
+            Console.WriteLine($"{systolicpressure}/{dialosticpressure} high");
+        }
+        else if (systolicpressure < 140 || dialosticpressure < 90)
+        {
+            Console.WriteLine($"{systolicpressure}/{dialosticpressure} stage 1 hypertension");
+        }
+        else
+        {
+            Console.WriteLine($"{systolicpressure}/{dialosticpressure} stage 2 hypertension");
+        }
+    }
 }
