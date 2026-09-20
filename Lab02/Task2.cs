77b32790ae515b0e74cb879242f766260e7d9ba9
Nest772 <nestkhomra@gmail.com>
2026-09-20 13:46:21 +0300

    Lab02 Task02

---
+++ Lab02/Task2.cs
@@ -0,0 +1,41 @@
+using System;
+
+System.Threading.Thread.CurrentThread.CurrentCulture =
+System.Globalization.CultureInfo.InvariantCulture;
+
+public static class Task2
+{
+    public static void Run()
+    {
+
+        
+        int N = int.Parse(Console.ReadLine());
+        int[] price = new int[N];
+
+        
+        for (int i = 0; i < N; i++)
+        {
+            price[i] = int.Parse(Console.ReadLine());
+        }
+
+        string before = string.Join(" ", price);
+        Console.WriteLine($"Before: {before}");
+
+        for (int i = 0; i < N - 1; i++)
+        {
+            for (int j = 0; j < N - 1 - i; j++)
+            {
+                if (price[j] > price[j + 1])
+                {
+                    (price[j], price[j + 1]) = (price[j + 1], price[j]);
+                }
+            }
+        }
+
+        string after = string.Join(" ", price);
+        Console.WriteLine($"After: {after}");
+
+        Console.WriteLine($"the Cheapest: {price[0]} grn");
+        Console.WriteLine($"The most expensive: {price[^1]} grn");
+    }
+}
